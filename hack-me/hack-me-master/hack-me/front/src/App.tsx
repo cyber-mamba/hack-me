3b254924e225fe0421ab8e980988581a4deb1882
cyber-mamba <shindojin@outlook.com>
2023-12-19 18:05:05 +0000

    front-components is still developping...

--- hack-me/hack-me-master/hack-me/front/src/App.tsx
+++ hack-me/hack-me-master/hack-me/front/src/App.tsx
@@ -7,6 +7,8 @@ import { MyDropzoneBasic } from './FileUpload';
 import React, { Component } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
+import GridBackgroundWIthText from './Functions/GridBackgroundWithText';
+import SideNav from './Functions/LeftNavigationMenu';
 
 /*
 const darkTheme = createTheme({
@@ -18,15 +20,17 @@ const darkTheme = createTheme({
 
 const theme = createTheme({
   palette: {
-    mode: 'dark',
+    //mode: 'dark',
     primary: {
-      main: '#7fff00',
+      //main: '#7fff00',
+      main: '#FFFFFF',
     },
     secondary: {
       main: '#FFA500',
     },
     background: {
-      default: '#15202B'
+      //default: '#15202B'
+      default: '#FFFFFF'
     }
   },
 });
@@ -41,15 +45,32 @@ const styles = {
   },
 };
 */
+const texts_to_repeat = 'ABE Hiroshi';
+const name_of_home_page = texts_to_repeat;
+const repeat_count = 4;
+const texts = Array(repeat_count).fill(texts_to_repeat);
 
 class App extends Component {
   render(){
     return(
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <SideNav />
         <div>
-          <h1>Hack Me</h1>
-          <MyDropzoneBasic />
+          <h1 className="homePageTitle">{name_of_home_page}のホームページ</h1>
+        </div>
+        <table className="table1">
+          <td>
+            <div className="latestInfo">
+              ★★★　最新情報　★★★
+            </div>
+          </td>
+        </table>
+
+
+        
+        <div>
+          <GridBackgroundWIthText texts={texts} columns={repeat_count}/>
         </div>
       </ThemeProvider>
     )
@@ -57,3 +78,7 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+
